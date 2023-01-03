bc06d8c43d3984858cb1f26d235a022e810ceffc
Neka <nekoanchor@yahoo.fr>
2023-01-03 15:22:16 +0100

    setup: init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>InstaGen</h1>
+        <p>Redirection site token generation for Instagram.</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
